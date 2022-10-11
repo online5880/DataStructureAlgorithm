065a17ab5005e14301038eac5558b330dd9d61ec
online5880 <gjtjqkr1@naver.com>
2022-10-11 23:53:39 +0900

    초기 커밋.

---
+++ Maze/Maze.cpp
@@ -0,0 +1,40 @@
+﻿// Maze.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include "pch.h"
+#include <iostream>
+#include "ConsoleHelper.h"
+
+int main()
+{
+	uint64 lastTick = 0;
+	while (true)
+	{
+#pragma region 프레임 관리
+		const uint64 currentTick = ::GetTickCount64();
+		const uint64 deltaTick = currentTick - lastTick;
+		lastTick = currentTick;
+#pragma endregion
+
+		// 입력
+
+		// 로직
+
+		// 렌더링
+
+		ConsoleHelper::SetCursorPosition(0, 0);
+		ConsoleHelper::ShowConsoleCursor(false);
+		ConsoleHelper::SetCursorColor(ConsoleColor::RED);
+
+		const char* TILE = "■";
+		
+		for(int32 y = 0; y < 25; y++)
+		{
+			for(int32 x = 0; x <25; x++)
+			{
+				cout << TILE;
+			}
+			cout << endl;
+		}
+	}
+}
---
+++ Maze/Types.h
@@ -0,0 +1,10 @@
+#pragma once
+
+using int8		= _int8;
+using int16		= _int16;
+using int32		= _int32;
+using int64		= _int64;
+using uint8		= unsigned _int8;
+using uint16	= unsigned _int16;
+using uint32	= unsigned _int32;
+using uint64	= unsigned _int64;
