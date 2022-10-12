6190a33062587481e1ea7ef9bb705fb154b9bc9f
online5880 <gjtjqkr1@naver.com>
2022-10-12 22:36:33 +0900

    맵 만들기

--- Maze/Maze.cpp
+++ Maze/Maze.cpp
@@ -3,10 +3,18 @@
 
 #include "pch.h"
 #include <iostream>
-#include "ConsoleHelper.h"
+#include "Board.h"
+#include "Player.h"
+
+Board board;
+Player player;
 
 int main()
 {
+	::srand(static_cast<unsigned int>(time(nullptr)));
+	board.Init(25, &player);
+	player.Init(&board);
+
 	uint64 lastTick = 0;
 	while (true)
 	{
@@ -19,22 +27,9 @@ int main()
 		// 입력
 
 		// 로직
+		player.Update(deltaTick);
 
 		// 렌더링
-
-		ConsoleHelper::SetCursorPosition(0, 0);
-		ConsoleHelper::ShowConsoleCursor(false);
-		ConsoleHelper::SetCursorColor(ConsoleColor::RED);
-
-		const char* TILE = "■";
-		
-		for(int32 y = 0; y < 25; y++)
-		{
-			for(int32 x = 0; x <25; x++)
-			{
-				cout << TILE;
-			}
-			cout << endl;
-		}
+		board.Render();
 	}
 }
--- Maze/pch.h
+++ Maze/pch.h
@@ -5,3 +5,44 @@
 #include <vector>
 #include "Types.h"
 using namespace std;
+
+struct Pos
+{
+	bool operator==(Pos& other)
+	{
+		return y == other.y && x == other.x;
+	}
+
+	bool operator!=(Pos& other)
+	{
+		return !(*this == other);
+	}
+
+	Pos operator*(Pos& other)
+	{
+		Pos ret;
+		ret.y = y + other.y;
+		ret.x = x + other.x;
+		return *this;
+	}
+
+	Pos& operator+=(Pos& other)
+	{
+		x += other.x;
+		y += other.y;
+		return *this;
+	}
+
+	int32 y = 0;
+	int32 x = 0;
+};
+
+enum Dir
+{
+	DIR_UP = 0,
+	DIR_LEFT = 1,
+	DIR_DOWN = 2,
+	DIR_RIGHT = 3,
+
+	DIR_COUNT = 4
+};
