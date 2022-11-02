235bf87bfb0e7525e2fe73d731f89411b65c6255
online5880 <gjtjqkr1@naver.com>
2022-11-02 22:31:52 +0900

    Stack

---
+++ Stack/Stack.cpp
@@ -0,0 +1,67 @@
+﻿// Stack.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+using namespace std;
+
+// Stack (LIFO Last-In-First-Out 후입선출)
+
+// [1][2][3][4]
+// 되돌리기 (Ctrl+Z)
+
+// [1]<->[2]<->[3]
+
+template<typename T, typename Container = vector<T>>
+class Stack
+{
+public:
+    void push(const T& value)
+    {
+        _container.push_back(value);
+    }
+
+    void pop()
+    {
+        _container.pop_back();
+    }
+
+    T& top()
+    {
+        return _container.back();
+    }
+
+    bool empty() { return _container.empty(); }
+    int size() { return _container.size(); }
+
+private:
+    //vector<T> _container;
+    //list<T> _container;
+    Container _container;
+};
+
+int main()
+{
+    Stack<int, list<int>> s;
+
+    // 삽입
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    // 비었나?
+    while (s.empty() == false)
+    {
+        // 최상위 원소
+        int data = s.top();
+        // 최상위 원소 삭제
+        s.pop();
+
+        cout << data << endl;
+    }
+
+    int size = s.size();
+}
+
